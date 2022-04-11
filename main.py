fbdcf8a9896af95b67e6842331ef247e314ef7a7
Julie Ho <julieho.98@gmail.com>
2022-04-10 19:18:30 -0700

    add bot command to send a random inspirational quote to the discord channel

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-# # # # # # # # # # # # # #
-#         PERIBOT         #
-# # # # # # # # # # # # # #
-
 import os
+import requests
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-    for guild in client.guilds:
-        print(f'Member of {guild.name} (id:{guild.id})')
-        print('Members:')
-        for member in guild.members:
-            print(f'{member.name}')
+bot = commands.Bot(command_prefix="!")
 
 
-client.run(TOKEN)
+@bot.command(name='inspo', help='Responds with a random inspirational quote')
+async def inspirational_quote(ctx):
+    request = requests.request('get', 'https://zenquotes.io?api=random')
+    response = request.json()[0]
+    quote = f":sparkles: *{response['q']}* :sparkles:"
+    author = f"\u2014 {response['a']}"
+
+    embed_msg = discord.Embed(title=quote, colour=0xAEE1D3)
+    embed_msg.set_footer(text=author)
+
+    await ctx.send(embed=embed_msg)
+
+
+bot.run(TOKEN)
